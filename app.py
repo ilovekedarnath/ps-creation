a5eaf31387a1d1d83df7d80e8f630c53d90543df
ilovekedarnath <parths6028@gmail.com>
2025-06-21 14:45:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -260,5 +260,4 @@ if __name__ == '__main__':
     from os import environ
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=int(environ.get('PORT', 5000)))
-
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
